61fc5c28aa068b1d77718d95d63c8fc31bfd0e92
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-10-08 23:04:57 +0530

    feat: implement the theme colors and size

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Zynk | Charge. Connect. Go. ",
   description: "All in one place for EV owners in Sri Lanka",
+};
+
+export const viewport: Viewport = {
+  width: 393,
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         {children}
       </body>
